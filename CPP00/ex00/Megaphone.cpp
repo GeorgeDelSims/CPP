08d8c5cf55d23bf2e7e08ea9751135498c5deb6d
George Sims <gsims@c3r3s5.42lausanne.ch>
2024-08-21 14:14:03 +0200

    megaphone still needs to remove spaces in quotes

--- CPP00/ex00/Megaphone.cpp
+++ CPP00/ex00/Megaphone.cpp
@@ -12,24 +12,25 @@ int	main (int ac, char *av[])
 	int				i;
 	size_t			j;
 	std::string		concatenated_str;
+	std::string		next_str;
 
 	if (ac < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
 		for (i = 1; i < ac; ++i)
-		{	
-			concatenated_str += av[i];
-			if (std::concatenated_str.back() != ' ')
-				concatenated_str += " ! ";
+		{
+			next_str = av[i];
+			while(next_str[0] == ' ')
+				next_str.erase(0, 1);
+			while (next_str[next_str.length() - 1] == ' ')
+				next_str.erase(next_str.length() - 1, 1);
+			concatenated_str += next_str;
+			concatenated_str += " ";
 		}
 	}
-	
 	for(j = 0; j < concatenated_str.length(); ++j)
-	{
 		concatenated_str[j] = std::toupper(static_cast<unsigned char>(concatenated_str[j]));
-	}
-
     std::cout << concatenated_str << std::endl;
 
 	return (0);
